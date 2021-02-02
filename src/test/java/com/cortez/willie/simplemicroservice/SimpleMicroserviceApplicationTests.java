f8be1044c382bf33ddab9e9144defff50d0b8911
agusu3537 <wc10@tampabay.rr.com>
2021-02-01 22:24:30 -0500

    Initial commit

---
+++ src/test/java/com/cortez/willie/simplemicroservice/SimpleMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cortez.willie.simplemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
